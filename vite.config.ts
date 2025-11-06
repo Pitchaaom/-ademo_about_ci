48bc72d25f1903b4e37224726eb55fca5d2ee6a4
pitchaaom <pitchaya6470@gmail.com>
2025-11-06 13:10:30 +0700

    FIX: Add resolve path alias to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-// filepath: demo-se2-frontend/vitest.config.ts
 import { defineConfig } from 'vitest/config';
+import path from 'path'; // <-- เพิ่มบรรทัดนี้
 
 export default defineConfig({
-test: {
-environment: 'jsdom',
-globals: true,
-},
+  test: { // <-- นี่คือของเดิมจาก Lab 3 
+    environment: 'jsdom',
+    globals: true,
+  },
+  resolve: { // <-- นี่คือ "ตัวแก้" ที่เราเพิ่มเข้าไป
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
