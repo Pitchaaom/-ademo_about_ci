5e13ad148f95104c5fe0994c2d07f7684618b705
pitchaaom <pitchaya6470@gmail.com>
2025-11-06 13:08:10 +0700

    Final fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-// filepath: demo-se2-frontend/vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+// filepath: demo-se2-frontend/vitest.config.ts
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+test: {
+environment: 'jsdom',
+globals: true,
+},
+});
